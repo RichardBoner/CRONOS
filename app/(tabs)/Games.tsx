0d10f50b7677d67a74fb24747247f9f1298daa15
Төгс-Эрдэнэ Отгонбүрэн <HP238706@G302-CC0108.local>
2024-02-26 19:02:37 +0800

    feat: basic frontend setup/frame
    added a fetcher and basic navigation
    
    "patch#1"

--- app/(tabs)/Games.tsx
+++ app/(tabs)/Games.tsx
@@ -4,8 +4,8 @@ import { View } from 'react-native';
 export default function GamesScreen(): React.ReactNode {
   return (
     <View>
-      <Link href={`/Game/1`} >game with id</Link>
-      <Link href={`/GamesCat/1`} >games with id</Link>
+      <Link href="/Game/1">game with id</Link>
+      <Link href="/GamesCat/1">games with id</Link>
     </View>
   );
 }
